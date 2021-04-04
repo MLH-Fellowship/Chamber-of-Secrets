6caaad4344f48d727cc57eefd00137e626d36a8d
Suhani Chawla <suhanichawla2000@gmail.com>
2021-04-04 16:55:30 +0530

    dropbox frontend for testing

--- src/components/googleAuth.js
+++ src/components/googleAuth.js
@@ -5,12 +5,19 @@ import { apiCall } from '../services/api';
 class GoogleAuthScreen extends React.Component {
   state={
     googleAuth:false,
-    googleCode:''
+    dropboxAuth:false,
+    googleCode:'',
+    dropboxCode:''
   }
   
   authGoogle = async () => {
     var uri=await apiCall("get",'/authenticate/get-gauth-url/')
-    console.log(this.props)
+    const newWindow = window.open(uri, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  };
+
+  authDropbox = async () => {
+    var uri=await apiCall("get",'/authenticate/get-dropbox-auth-url/')
     const newWindow = window.open(uri, '_blank', 'noopener,noreferrer')
     if (newWindow) newWindow.opener = null
   };
@@ -26,6 +33,18 @@ class GoogleAuthScreen extends React.Component {
       })
     }
   }
+
+  submitDropboxCode=async(e)=>{
+    e.preventDefault()
+    var data={code:this.state.dropboxCode}
+    var status=await apiCall("post","/authenticate/set-dropbox-auth-token/",data)
+    console.log(status)
+    if(status=="Dropbox Authentication Successful"){
+      this.setState({
+        dropboxAuth:true
+      })
+    }
+  }
   
   onChange=(e)=>{
     this.setState({[e.target.name]:e.target.value})
@@ -35,14 +54,21 @@ class GoogleAuthScreen extends React.Component {
     return (
         <div>
           <button onClick={this.authGoogle}>Google Oauth</button>
-          {this.state.googleAuth ? 
+          <button onClick={this.authDropbox}>Dropbox Oauth</button>
+          {this.state.googleAuth && this.state.dropboxAuth ? 
             //  <p>google auth done</p>
             <Redirect to="/upload"></Redirect>
              :
+             <>
              <form onSubmit={this.submitGoogleCode}>
               <input onChange={this.onChange} type="text" name="googleCode" />
               <input type="submit"/>
             </form>
+             <form onSubmit={this.submitDropboxCode}>
+             <input onChange={this.onChange} type="text" name="dropboxCode" />
+             <input type="submit"/>
+           </form>
+           </>
           }
         </div>
     );
