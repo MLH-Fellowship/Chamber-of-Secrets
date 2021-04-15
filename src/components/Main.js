1f3c24fbf8b99a96bd443990c0b105d4f0d741d3
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-15 16:58:16 +0530

    #21 issue fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AuthForm from './AuthForm';
-import { Switch, Route, withRouter } from "react-router-dom"
+import { Switch, Route, withRouter ,Redirect} from "react-router-dom"
 import Upload from './Upload';
 import GoogleAuthScreen from './Oauth'
 import { setTokenHeader } from '../services/api';
@@ -14,10 +14,10 @@ const Main = (props) => {
     return (
         <div>
             <Switch>
-                <Route exact path="/" render={props => isLoggedIn ? <Upload {...props} /> : <AuthForm {...props} />}></Route>
-                <Route exact path="/upload" render={props => <Upload {...props} />}></Route>
-                <Route exact path="/OAuth" render={props => <GoogleAuthScreen {...props} />}></Route>
-                <Route exact path="/privateKey" render={props => <PrivateKeyScreen {...props} />}></Route>
+            <Route exact path="/" render={props => isLoggedIn ? <Upload {...props} /> : <AuthForm {...props}/>}></Route>
+            <Route exact path="/upload" render={props => isLoggedIn ? <Upload {...props} /> : <Redirect to="/"/> }></Route>
+            <Route exact path="/OAuth" render={props => isLoggedIn? <GoogleAuthScreen {...props}/>  : <Redirect to="/"/>}></Route>
+            <Route exact path="/privateKey" render={props => isLoggedIn? <PrivateKeyScreen {...props} />  : <Redirect to="/"/>}></Route>
             </Switch>
         </div>
     )
